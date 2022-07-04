b26800a45d88334dad1a9868fa722bba123d2997
JustAnotherJavaProgrammer <justanotherjavaprogrammer@users.noreply.github.com>
2022-07-04 10:42:52 +0200

    initial commit

---
+++ src/main/java/de/lukas/imagenetwork/ImageNetworkBackendApplication.java
@@ -0,0 +1,13 @@
+package de.lukas.imagenetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageNetworkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageNetworkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/lukas/imagenetwork/ImageNetworkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.lukas.imagenetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageNetworkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
