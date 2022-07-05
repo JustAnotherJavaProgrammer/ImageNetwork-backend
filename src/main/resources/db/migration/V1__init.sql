7ed97a24e881ea7d1e1849b45153b4c587830b49
JustAnotherJavaProgrammer <justanotherjavaprogrammer@users.noreply.github.com>
2022-07-05 11:24:12 +0200

    Merged the migration files

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,3 +1,4 @@
+-- Users
 CREATE TABLE users
   (
      id         BIGSERIAL NOT NULL,
@@ -15,3 +16,34 @@ CREATE TABLE users
     ADD CONSTRAINT make_unique UNIQUE (name, email, nickname, password);
   ALTER TABLE users
     ALTER COLUMN deleted SET DEFAULT FALSE;
+
+-- Posts
+CREATE TABLE posts (
+    id BIGSERIAL NOT NULL,
+    userId BIGINT NOT NULL,
+    image VARCHAR(255),
+    comment VARCHAR(255),
+    title VARCHAR(255),
+    createdAt TIMESTAMP WITH TIME ZONE NOT NULL,
+    modifiedAt TIMESTAMP WITH TIME ZONE,
+    deleted BOOLEAN NOT NULL,
+    PRIMARY KEY(id)
+);
+
+ALTER TABLE posts
+    ADD CONSTRAINT user_id FOREIGN KEY(userId) REFERENCES users(id);
+ALTER TABLE posts
+    ALTER COLUMN deleted SET DEFAULT FALSE;
+
+-- Friends
+CREATE TABLE friends (
+    id BIGSERIAL NOT NULL,
+    userId BIGINT NOT NULL,
+    friendId BIGINT NOT NULL,
+    PRIMARY KEY(id)
+);
+
+ALTER TABLE friends
+    ADD CONSTRAINT user_ids FOREIGN KEY(userId) REFERENCES users(id);
+ALTER TABLE friends
+    ADD CONSTRAINT friend_ids FOREIGN KEY(friendId) REFERENCES users(id);
