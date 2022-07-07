4f93b4f763db97a445c6286e77f584ab34e53fd7
JustAnotherJavaProgrammer <justanotherjavaprogrammer@users.noreply.github.com>
2022-07-07 17:27:00 +0200

    Added a CORS mapping to allow CORS requests during development

--- src/main/java/de/lukas/imagenetwork/security/WebSecurity.java
+++ src/main/java/de/lukas/imagenetwork/security/WebSecurity.java
@@ -4,12 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.NonNullApi;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.sql.DataSource;
 
@@ -20,11 +26,21 @@ public class WebSecurity {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests();
         // TODO: remove csrf.disable(), if possible
-        http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, "/user")
+        http.cors().and().csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, "/user")
                 .permitAll()
                 .anyRequest()
                 .authenticated().and()
                 .httpBasic();
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**").allowedOriginPatterns("*").allowCredentials(true);
+            }
+        };
     }
 }
